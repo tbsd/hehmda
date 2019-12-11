92ec0da880ad307f887d8483fe3d71640b4efa4a
Trankal <temasabakov@yandex.ru>
2019-12-11 21:54:16 +0300

    Data change (utils.py)

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 # return ifnoramtion about user or empty document if no such token in base
 def validate_session(users, request):
     if 'session' in request.cookies:
@@ -7,5 +11,11 @@ def validate_session(users, request):
 
 
 # adds obj to array field in document with id in collection
-def push_to_db(collection, id, field, obj):
-    collection.find_one_and_update({'id': id}, {'$push': {field: obj}})
+def push_to_db(collection, ide, field, obj):
+    collection.find_one_and_update({'id': ide}, {'$push': {field: obj}})
+
+
+# return string of random letters and digits
+def random_string(string_length=20):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for i in range(string_length))
