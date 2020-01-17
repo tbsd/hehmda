db387d1c43d2817750213e09c79d3dcc9e317ad8
Anton Kurvatov <a.kurbatov@protonmail.com>
2020-01-17 15:13:52 +0300

    add argument to change push place

--- utils.py
+++ utils.py
@@ -13,8 +13,9 @@ def validate_session(users, request):
 
 
 # adds obj to array field in document with id in collection
-def push_to_db(collection, id, field, obj):
-    collection.find_one_and_update({'id': id},
+def push_to_db(collection, id, field, obj, to_front=True):
+    if to_front:
+        collection.find_one_and_update({'id': id},
                                     {'$push': {
                                         field: {
                                             '$each': [obj],
@@ -22,7 +23,9 @@ def push_to_db(collection, id, field, obj):
                                             }
                                         }
                                     })
-
+    else:
+        collection.find_one_and_update({'id': id}, {'$push': {field: obj}})
+		
 
 # return string of random letters and digits
 def random_string(string_length=10):
